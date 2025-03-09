edfff4379a452ca56ca28758632cec6f1c066c08
varshadhanotiya <varshadhanotiya7026@gmail.com>
2025-03-09 11:51:57 +0530

    connect express app to mongoDB database

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
+const connectDB = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
+
+connectDB();
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
